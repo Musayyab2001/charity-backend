2efc66d529800bee7fa4cf4ba26d187f33d89523
Musayyab Naveed <mussayyab2001@gmail.com>
2019-04-21 00:09:58 +0200

    Created Model -a

---
+++ database/factories/CityBasicDataFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Model\CityBasicData;
+use Faker\Generator as Faker;
+
+$factory->define(CityBasicData::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
