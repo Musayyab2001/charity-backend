dcfac73502aebdb6bdf7308abf6eae06c9d7eeca
Musayyab Naveed <mussayyab2001@gmail.com>
2019-04-21 17:46:20 +0200

    Relationship created

--- app/Model/CityBasicData.php
+++ app/Model/CityBasicData.php
@@ -2,9 +2,19 @@
 
 namespace App\Model;
 
+use App\Model\CharityReciver;
+use App\Model\Sponsor;
 use Illuminate\Database\Eloquent\Model;
 
 class CityBasicData extends Model
 {
-    //
+    public function sponsors()
+    {
+        return $this->hasMany(Sponsor::class);
+    }
+
+    public function charityReciver()
+    {
+        return $this->hasMany(CharityReciver::class);
+    }
 }
--- app/Model/Sponsor.php
+++ app/Model/Sponsor.php
@@ -2,9 +2,13 @@
 
 namespace App\Model;
 
+use App\Model\CityBasicData;
 use Illuminate\Database\Eloquent\Model;
 
 class Sponsor extends Model
 {
-    //
+    public function cityBasicData()
+    {
+        return $this->belongsTo(CityBasicData::class);
+    }
 }
