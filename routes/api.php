9340ad58fed4b0bc2431eda9249fca6b2e699c9a
Musayyab Naveed <mussayyab2001@gmail.com>
2019-04-21 20:29:26 +0200

    CityBasicData Resource created

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::Resource('/Citybasicdata', 'CityBasicDataController');
+Route::Resource('citybasicdata', 'CityBasicDataController');
 
-Route::Resource('/charityrecivers', 'CharityReciverController');
+Route::Resource('charityrecivers', 'CharityReciverController');
 
 Route::Resource('sponsors', 'SponsorController');
