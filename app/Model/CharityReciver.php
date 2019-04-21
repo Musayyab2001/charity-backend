e95a69309917596df505b676b47962e6ab26e497
Musayyab Naveed <mussayyab2001@gmail.com>
2019-04-21 17:46:20 +0200

    Relationship created

--- app/Model/CharityReciver.php
+++ app/Model/CharityReciver.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class CharityReciver extends Model
 {
-    //
+    public function cityBasicData()
+    {
+        return $this->belongsTo(CityBasicData::class);
+    }
 }
